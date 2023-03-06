fd2e182d56091a212fa56bc133ed37dbfb755c11
Hasnannn <hasnanh.7113@gmail.com>
2023-03-06 08:17:12 +0700

    mod 3

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,86 @@ namespace modul2_1302210100
         public Form1()
         {
             InitializeComponent();
+        }
+        int angka1 = 0;
+        int angka2 = 0;
+        int hasil = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            label1.Text = label1.Text + "1";
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(label1.Text);
+            label1.Text = "";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "0";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "9"; ;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "4";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "3";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(label1.Text);
+            angka2 = int.Parse(label1.Text);
+            label1.Text = angka1 + " " + angka2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "2";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            
         }
     }
 }
